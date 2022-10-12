1eb83cd856b95aac7f058661652d1021e076daec
Oskar Skowroński <97964979+OskarSko@users.noreply.github.com>
2022-10-12 13:30:30 +0200

    first models

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'to_do_app.apps.ToDoAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- to_do_app/models.py
+++ to_do_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class List(models.Model):
+    list_name = models.CharField(max_length=200)
+    dateCreate = models.DateTimeField("Stworzono")
+    dateToFinish = models.DateTimeField("Deadline")
+
+class Task(models.Model):
+    list = models.ForeignKey(List, on_delete=models.CASCADE)
+    taskName = models.CharField(max_length=200)
+    taskDescription = models.CharField(max_length=200)
+    order = models.IntegerField(default=0)
