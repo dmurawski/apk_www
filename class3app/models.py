676c9e14c6cafe13c0f8ac9dfa83bd4d47dc0fb4
Damian Murawski <l.com>
2022-10-19 13:16:06 +0200

    Zadanie_1

--- class3app/models.py
+++ class3app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Osoba(models.Model):
+    imie = models.CharField(max_length=60 ,blank=False)
+    nazwisko = models.CharField(max_length=60, blank=False)
+    MONTHS = (
+        ('I','styczeń'),
+        ('II','luty'),
+        ('III','marzec'),
+        ('IV','kwiecień'),
+        ('V','maj'),
+        ('VI','czerwiec'),
+        ('VII','lipiec'),
+        ('VIII','sierpień'),
+        ('XI','wrzesień'),
+        ('X','październik'),
+        ('XI','listopad'),
+        ('XII','grudzień'),
+    )
+    miesiac_urodzenia = models.CharField(max_length=4, choices=MONTHS)
+
