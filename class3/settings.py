29c17ec95c9abe85c73e3b0a1dab7cd36b12b1c1
Damian Murawski <l.com>
2022-10-19 13:26:36 +0200

    Zadanie_1_v2\

--- class3/settings.py
+++ class3/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'class3app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'class3app.apps.Class3AppConfig',
 ]
 
 MIDDLEWARE = [
