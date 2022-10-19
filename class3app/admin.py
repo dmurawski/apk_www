531d6fa330af57a741b33fec924de4c81b1824a9
Damian Murawski <l.com>
2022-10-19 14:24:34 +0200

    Zadanie_8

--- class3app/admin.py
+++ class3app/admin.py
@@ -3,9 +3,15 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Osoba
-
+from .models import Druzyna
 class OsobaAdmin(admin.ModelAdmin):
-    list_display = ['imie', 'nazwisko','miesiac_urodzenia','data_dodania']
+    list_display = ['imie', 'nazwisko','miesiac_urodzenia','data_dodania','druzyna']
 
 # ten obiekt też trzeba zarejestrować w module admin
 admin.site.register(Osoba, OsobaAdmin)
+
+class DruzynaAdmin(admin.ModelAdmin):
+    list_display = ['nazwa', 'kraj']
+
+# ten obiekt też trzeba zarejestrować w module admin
+admin.site.register(Druzyna, DruzynaAdmin)
--- class3app/models.py
+++ class3app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Druzyna(models.Model):
+    nazwa = models.CharField(max_length=40)
+    kraj = models.CharField(max_length=2, blank=True, null=True)
+    def __str__(self):
+        return self.nazwa
+
+
 class Osoba(models.Model):
     imie = models.CharField(max_length=60 ,blank=False)
     nazwisko = models.CharField(max_length=60, blank=False)
@@ -20,12 +27,11 @@ class Osoba(models.Model):
     )
     miesiac_urodzenia = models.CharField(max_length=4, choices=MONTHS, default=MONTHS[0])
     data_dodania = models.DateField(auto_now=True)
+    druzyna = models.ForeignKey(Druzyna, on_delete=models.SET_NULL,null=True)
 
     def __str__(self):
         return self.imie+" "+self.nazwisko
 
     class Meta:
         ordering = ["nazwisko"]
-
-
 
