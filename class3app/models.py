83fec7216664f4eb7917323dca1e61f9b89fbaf0
dmurawski <dmurawski018@gmail.com>
2022-10-23 18:23:02 +0200

    Zadanie_9

--- class3app/models.py
+++ class3app/models.py
@@ -5,7 +5,7 @@ class Druzyna(models.Model):
     nazwa = models.CharField(max_length=40)
     kraj = models.CharField(max_length=2, blank=True, null=True)
     def __str__(self):
-        return self.nazwa
+        return self.nazwa+" "+self.kraj
 
 
 class Osoba(models.Model):
@@ -27,7 +27,7 @@ class Osoba(models.Model):
     )
     miesiac_urodzenia = models.CharField(max_length=4, choices=MONTHS, default=MONTHS[0])
     data_dodania = models.DateField(auto_now=True)
-    druzyna = models.ForeignKey(Druzyna, on_delete=models.SET_NULL,null=True)
+    druzyna = models.ForeignKey(Druzyna, on_delete=models.SET_NULL,null=True,verbose_name="Drużyna")
 
     def __str__(self):
         return self.imie+" "+self.nazwisko
