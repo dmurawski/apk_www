a43d9a09fbe60609bd34bbed709581f25f5cdc54
dmurawski <dmurawski018@gmail.com>
2022-10-23 18:12:17 +0200

    Zadanie_10

--- class3app/admin.py
+++ class3app/admin.py
@@ -6,12 +6,13 @@ from .models import Osoba
 from .models import Druzyna
 class OsobaAdmin(admin.ModelAdmin):
     list_display = ['imie', 'nazwisko','miesiac_urodzenia','data_dodania','druzyna']
-
+    list_filter = ('druzyna','data_dodania' )
 # ten obiekt też trzeba zarejestrować w module admin
 admin.site.register(Osoba, OsobaAdmin)
 
 class DruzynaAdmin(admin.ModelAdmin):
     list_display = ['nazwa', 'kraj']
+    list_filter = ('kraj',)
 
 # ten obiekt też trzeba zarejestrować w module admin
 admin.site.register(Druzyna, DruzynaAdmin)
